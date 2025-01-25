c03b03c7d72a88e054a1af57165fea71dc34a942
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-01-25 00:19:06 -0300

    refactor: refactor table rows

--- src/Components/Home/StudentsTable.tsx
+++ src/Components/Home/StudentsTable.tsx
@@ -1,38 +1,41 @@
-  import { Box, Table, For, VStack, Button } from "@chakra-ui/react";
-  import { FaRegTrashAlt } from "react-icons/fa";
+import { Box, Table, For, VStack, Button } from "@chakra-ui/react";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 
-  import students from "../../data";
+import students from "../../data";
 
-  export default function StudentsTable() {
-    return (
-      <VStack padding={{base: "20px 20px 20px 20px", md:"43px 66px 43px 66px"}}>
-        <Table.Root maxW={"1310px"}>
-          <Table.Header>
-            <Table.Row justifyContent={"space-around"} borderBottom={"solid 1px #0000001f"}>
-              <Table.ColumnHeader  textAlign={"center"} >nome</Table.ColumnHeader>
-              <Table.ColumnHeader textAlign={"center"}>idade</Table.ColumnHeader>
-              <Table.ColumnHeader textAlign={"center"}>turma</Table.ColumnHeader>
-              <Table.ColumnHeader textAlign={"center"}>deletar</Table.ColumnHeader>
+export default function StudentsTable() {
+  return (
+    <VStack padding={{ base: "20px 20px 20px 20px", md: "43px 66px 43px 66px" }}>
+      <Table.Root maxW={"1310px"}>
+        <Table.Header>
+          <Table.Row justifyContent={"space-around"} borderBottom={"solid 1px #0000001f"}>
+            <For each={["nome", "idade", "turma", "deletar"]}>
+              {(title) => (
+                <Table.ColumnHeader key={title} textAlign={"center"} >{title}</Table.ColumnHeader>
+              )}
+            </For>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {students.map((student) => (
+            <Table.Row key={student.id} bgColor={"white"} color={"black"} alignItems={"center"} justifyContent={"space-around"} borderBottom={"solid 1px #0000001f"}>
+              <For each={["name", "age", "class"]}>
+                {(key)=>(
+                  <Table.Cell key={key} textAlign={"center"} >{student[key]}</Table.Cell>
+                )}
+              </For>
+              <Table.Cell display={"flex"} textAlign={"center"} justifyContent={"center"}>
+                <FaRegTrashAlt />
+              </Table.Cell>
             </Table.Row>
-          </Table.Header>
+          ))}
 
-          <Table.Body>
-            {students.map((student)=>(
-                <Table.Row key={student.id} bgColor={"white"} color={"black"} alignItems={"center"} justifyContent={"space-around"} borderBottom={"solid 1px #0000001f"}>
-                  <Table.Cell textAlign={"center"} >{student.name}</Table.Cell>
-                  <Table.Cell textAlign={"center"}>{student.age}</Table.Cell>
-                  <Table.Cell textAlign={"center"}>{student.class}</Table.Cell>
-                  <Table.Cell display={"flex"} textAlign={"center"} justifyContent={"center"}>
-                    <FaRegTrashAlt />
-                  </Table.Cell>
-                </Table.Row>
-            ))}
+        </Table.Body>
 
-          </Table.Body>
+      </Table.Root>
 
-        </Table.Root>
-
-      </VStack>
-    )
-  }
+    </VStack>
+  )
+}
